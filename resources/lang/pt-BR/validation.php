3979ca5dc280c1753a91cb0aff141feade1df27e
constpereiradev <amanda.c.pereira@ba.estudante.senai.br>
2023-04-20 14:09:00 -0300

    tradução de validação

---
+++ resources/lang/pt-BR/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'required' => 'O campo :attribute é obrigatório.',
+    'min' => 'aa',
+    'email' => 'O campo :attribute deve ser um endereço de e-mail válido.',
+    'unique' => 'O :attribute já está sendo usado por outro usuário.',
+    'password.min' => 'A senha deve conter pelo menos 8 caracteres.',
+    'confirmed' => 'As senhas não conferem.',
+];
